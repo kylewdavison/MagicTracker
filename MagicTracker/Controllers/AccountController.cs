76e47b17368b97cf6601aff10c5c94c3dfd1fd0b
Kyle Davison <kyle.w.davison@gmail.com>
2019-07-09 11:46:29 -0400

    configured identitymodels

--- MagicTracker/Controllers/AccountController.cs
+++ MagicTracker/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using MagicTracker.Models;
+using MagicTracker.Data;
 
 namespace MagicTracker.Controllers
 {
