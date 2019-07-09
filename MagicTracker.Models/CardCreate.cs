bbb93299c08c35bef36e04c6bac158a949c449af
Kyle Davison <kyle.w.davison@gmail.com>
2019-07-09 13:57:52 -0400

    added create card controller/model

---
+++ MagicTracker.Models/CardCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicTracker.Models
+{
+    public class CardCreate
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "Please enter at least 3 characters.")]
+        [MaxLength(40, ErrorMessage = "There are too many characters in this field.")]
+        public string CardName { get; set; }
+        public int MultiverseId { get; set; }
+    }
+}
--- MagicTracker/Controllers/CollectionController.cs
+++ MagicTracker/Controllers/CollectionController.cs
@@ -16,5 +16,22 @@ namespace MagicTracker.Controllers
             var model = new CollectionItem[0];
             return View(model);
         }
+
+        //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CardCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
     }
 }
