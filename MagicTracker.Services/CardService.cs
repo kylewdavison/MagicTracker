51d36ddac51fe92e53adf277a86bb32f9fb41703
Kyle Davison <kyle.w.davison@gmail.com>
2019-07-09 14:16:59 -0400

    card service setup

---
+++ MagicTracker.Services/CardService.cs
@@ -0,0 +1,54 @@
+﻿using MagicTracker.Data;
+using MagicTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicTracker.Services
+{
+    public class CardService
+    {
+        private readonly Guid _userId;
+
+        public CardService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateCard(CardCreate model)
+        {
+            var entity = new Card()
+            {
+                OwnerId = _userId,
+                Name = model.CardName
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Cards.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CollectionItem> GetCollection()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx
+                    .Cards
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                            new CollectionItem
+                            {
+                                CardId = e.CardId,
+                                CardName = e.Name
+                            }
+                    );
+                return query.ToArray();
+            }
+        }
+    }
+}
